84017df81372f4eddabfdb526bc536ccf0286d43
Akash AR <akuaka750@gmail.com>
2025-01-22 18:55:47 +0530

    first commit

---
+++ lib/models/category.dart
@@ -0,0 +1,26 @@
+
+
+
+
+import 'dart:ui';
+
+enum Categories{
+    vegetables,
+    fruit,
+    meat,
+    dairy,
+    carbs,
+    sweets,
+    spices,
+    convenience,
+    hygiene,
+    other
+}
+
+
+class Category {
+  const Category(this.title, this.color);
+
+  final String title;
+  final Color color;
+}
